1ba3dda1afa2f5fa3614ea06e8af22ecc34a7eaf
Anya Ganapathy <156939078+aganapathyy@users.noreply.github.com>
2024-02-03 23:34:43 +0000

    lab 4 complete

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function setup() {
   createCanvas(320, 240);
 }
+var x = 100, y = 100;
+var diameter = 100;
 
 function draw() {
   background(200);
@@ -8,11 +10,17 @@ function draw() {
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
-    
+    fill(10,80,70)
+    x = width / 3;
+    y = height / 3;
+    diameter = 200;
   }
   else { 
     // otherwise do this:
-    
+    fill(20,30,40)
+    x = width / 2;
+    y = height / 2;
+    diameter = 100;
   }
-  ellipse(width/2, height/2, 100, 100);
+  ellipse(x, y, diameter);
 }
